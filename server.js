979cc2c5836b464e8bbe8baab60974d0a1dd7bb3
Anusha <anushasharma824@gmail.com>
2024-01-27 09:57:10 +0530

    rest api added

---
+++ server.js
@@ -0,0 +1,67 @@
+import express from "express";
+import bodyParser from "body-parser";
+// import mongoose from "mongoose";
+// import MarksSchema from "./Schema.js";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = 3000;
+
+const data = [
+  { uid: "A001", sem1: 3.5, sem2: 3.8, cgpa: 3.65 },
+  { uid: "A002", sem1: 3.2, sem2: 3.6, cgpa: 3.4 },
+  { uid: "A003", sem1: 3.8, sem2: 4.0, cgpa: 3.9 },
+  { uid: "A004", sem1: 3.0, sem2: 3.5, cgpa: 3.25 },
+  { uid: "A005", sem1: 3.7, sem2: 3.9, cgpa: 3.8 },
+];
+
+app.get("/students", (req, res) => {
+  res.send(data);
+});
+
+app.post("/students", (req, res) => {
+  data.push(req.body);
+  res.send(req.body);
+});
+
+app.put("/students/:uid", (req, res) => {
+  const index = data.findIndex((student) => student.uid === req.params.uid);
+  if (index === -1) {
+    res.status(404).send("Student not found");
+  } else {
+    data[index] = { ...data[index], ...req.body };
+    res.send("Course updated");
+  }
+});
+
+app.patch("/students/v1/:uid", (req, res) => {
+  const index = data.findIndex((student) => student.uid === req.params.uid);
+  if (index === -1) {
+    res.status(404).send("Course not found");
+  } else {
+    const dataToUpdate = data[index];
+    // Update specific fields if they exist in the request body
+    if (req.body.sem1) dataToUpdate.sem1 = req.body.sem1;
+    if (req.body.sem2) dataToUpdate.sem2 = req.body.sem2;
+    if (req.body.cgpa) dataToUpdate.cgpa = req.body.cgpa;
+
+    res.send("Student data partially updated");
+  }
+});
+
+app.delete("/students/d1/:uid", (req, res) => {
+  const index = data.findIndex((student) => student.uid === req.params.uid);
+
+  if (index === -1) {
+    res.status(404).send("Student not found");
+  } else {
+    data.splice(index, 1);
+    res.send("Student data deleted");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`);
+});
